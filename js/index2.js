065fbbc8bfde77b9398c97d88da68a28682e6bd6
oma189 <ofaith189@gmail.com>
2022-09-28 13:20:20 +0100

    init commit

---
+++ js/index2.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//     $("#menuBlock div ul li a").click(function (e){
+//         e.preventDefault();
+//         var selected = $(this).attr('href');
+//         $( selected ).fadeIn('slow').siblings().hide();
+//         }
+
+// document.getElementById('aboutTab').addEventListener ("click", function(){
+//     document.getElementById('aboutTab').classList.remove(d-none)
+// })
+
+
